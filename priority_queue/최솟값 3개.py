9953890257c146a0a66c6a7a40ec96dcd18ce7c7
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-13 19:00:23 +0900

    Day15

--- priority_queue/최솟값 3개.py
+++ priority_queue/최솟값 3개.py
@@ -9,11 +9,24 @@ arr = list(map(int, input().split()))
 
 heap = []
 
-for num in arr:
-    heapq.heappush(heap, num)
-
+for i in range(n):
+    heapq.heappush(heap, arr[i])
     if len(heap) < 3:
         print(-1)
-        continue
+
+    else:
+        n1 = heapq.heappop(heap)
+        n2 = heapq.heappop(heap)
+        n3 = heapq.heappop(heap)
+
+        print(n1 * n2 * n3)
+
+        heapq.heappush(heap, n1)
+        heapq.heappush(heap, n2)
+        heapq.heappush(heap, n3)
+
+
+
+
 
 
