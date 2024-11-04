4b9d4bb1e0203d83950ff473e4941f42cd65e914
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-11-04 21:02:41 +0900

    Day2

--- programmers/hash/베스트앨범.py
+++ programmers/hash/베스트앨범.py
@@ -2,28 +2,25 @@ from collections import defaultdict
 
 
 def solution(genres, plays):
+    plays_sum = defaultdict(int)
+    g = defaultdict(list)
 
-    genre_total_play = defaultdict(int)
+    for i, (genre, play) in enumerate(zip(genres, plays)):
+        g[genre].append((play, i))
+        plays_sum[genre] += play
 
-    genre_songs = defaultdict(list)
+    for key in g:
+        g[key].sort(key=lambda x: (-x[0], x[1]))
 
-    for i in range(len(genres)):
-        genre_total_play[genres[i]] += plays[i]
-        genre_songs[genres[i]].append((plays[i], i))
-
-    sorted_genres = sorted(
-        genre_total_play.keys(), key=lambda x: genre_total_play[x], reverse=True
+    sorted_plays_sum = sorted(
+        plays_sum.keys(), key=lambda x: plays_sum[x], reverse=True
     )
 
     answer = []
-    # 2. 각 장르별로 노래 선택
-    for genre in sorted_genres:
-        # 장르 내 노래를 재생 횟수(내림차순)와 고유번호(오름차순)로 정렬
-        songs = sorted(genre_songs[genre], key=lambda x: (-x[0], x[1]))
 
-        # 장르별로 최대 2곡 선택
-        for i in range(min(len(songs), 2)):
-            answer.append(songs[i][1])
+    for genre in sorted_plays_sum:
+        for i in range(min(len(g[genre]), 2)):
+            answer.append(g[genre][i][1])
 
     return answer
 
